3d65c411ac0d108b05f0c8bc5443f23dc5574e4d
Fredy Companocca <fredy.companocca@tecsup.edu.pe>
2026-09-10 15:55:26 -0500

    style: update EjemploSwitch colors

--- app/src/main/java/com/example/lab04/EjemploSwitch.kt
+++ app/src/main/java/com/example/lab04/EjemploSwitch.kt
@@ -1,14 +1,20 @@
 package com.example.lab04
 
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun EjemploSwitch() {
     var activo by remember { mutableStateOf(false) }
-    Switch(checked = activo, onCheckedChange = { activo = it })
+    Switch(
+        checked = activo,
+        onCheckedChange = { activo = it },
+        colors = SwitchDefaults.colors(checkedThumbColor = Color(0xFF006400))
+    )
 }
